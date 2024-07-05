51afa53c4df8012cbe576a456f6e0f2212b31d69
zoonyoung <9851248@gmail.com>
2024-07-06 00:19:29 +0900

    🧹 chore(초기 파일 및 폴더 구조): 셋팅

--- src/app/api/utils.ts
+++
@@ -1,6 +0,0 @@
-import { db } from '@/service/kysely';
-
-export async function addUser({ id, name, email, image }) {
-  return db.insertInto().onConflict();
-}
-sd;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,5 @@
-"use client";
+const Home = () => {
+  return <div>Home</div>;
+};
 
-import Test from "@/components/Test/Test";
-import { SessionProvider, signIn, signOut } from "next-auth/react";
-export default function Home() {
-  // try {
-  //   const response = await fetch('http://localhost:3000/api/users/me', {
-  //     cache: 'no-store'
-  //   }).then(res => res.json());
-  //   console.log(response);
-  // } catch (e) {
-  //   console.log(e);
-  // }
-  //
-  // try {
-  //   const postResponse = await fetch('http://localhost:3000/api/auth/siginIn', {
-  //     method: 'POST',
-  //     cache: 'no-store',
-  //     headers: {
-  //       Authorization: 'asdfadsf'
-  //     }
-  //   }).then(res => res.json());
-  // } catch (e) {
-  //   console.log(e);
-  // }
-  //
-  const handleTest = async () => {
-    const response = await fetch("http://localhost:3000/api/auth/signIn").then((res) => res.json);
-    console.log(response);
-  };
-  return (
-    <SessionProvider>
-      <div>
-        <button type='button' onClick={() => signIn()}>
-          signIn
-        </button>
-        <p></p>
-        <button type='button' onClick={() => signOut()}>
-          logOut
-        </button>
-        <button type='button' onClick={handleTest}>
-          test
-        </button>
-        <Test />
-      </div>
-    </SessionProvider>
-  );
-}
+export default Home;
